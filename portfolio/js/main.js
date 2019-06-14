ac6bdd62419903f93b8e8e835fd80505df7feebe
Jeremy Dowe <jeremy.dowe@gmail.com>
2019-06-15 08:54:33 +1000

    main.js update checked
    Update to the JS code which uses the event listener change to toggle the Dark Mode switch.

--- portfolio/js/main.js
+++ portfolio/js/main.js
@@ -1,3 +1,11 @@
-document.getElementById('myOnOffSwitch').addEventListener('change', function(event){
-  (event.target.checked) ? document.body.setAttribute('data-theme', 'dark') : document.body.removeAttribute('data-theme');
+// Dark mode checkbox (change) event
+const darkmodeSwitch = document.querySelector('.OnOffSwitch');
+
+darkmodeSwitch.addEventListener('change', function(event){
+	 //event.target.checked TRUE or FALSE set the attribute on document body
+   if (event.target.checked){
+     document.body.setAttribute('data-theme', 'dark')
+   } else {
+     document.body.removeAttribute('data-theme')
+   }
 });
