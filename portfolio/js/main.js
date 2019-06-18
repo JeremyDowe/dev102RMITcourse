bab9ca22be3cc98493e36a97118f0122d25fc8d8
Jeremy Dowe <jeremy.dowe@gmail.com>
2019-06-18 18:43:16 +1000

    updates to prototype
    menu navigation is now sticky
    darkmode switch is now working with background image and colour changing.
    update font list.

--- portfolio/js/main.js
+++ portfolio/js/main.js
@@ -9,3 +9,22 @@ darkmodeSwitch.addEventListener('change', function(event){
      document.body.removeAttribute('data-theme')
    }
 });
+
+// OnScroll event listener for sticky navigation
+const documentTop = window.pageYOffset;
+
+window.addEventListener('scroll', function(event){
+  let mouse_y = window.pageYOffset;
+  let el = document.querySelector('nav');
+  if (mouse_y > 180){
+      el.style.position = 'fixed';
+      el.classList.add('sticky');
+      if (mouse_y == 520) {
+          el.style.position = 'absolute';
+          el.style.top = 0;
+      }
+  } else {
+      el.style.position = 'absolute';
+      el.classList.remove('sticky');
+  }
+});
