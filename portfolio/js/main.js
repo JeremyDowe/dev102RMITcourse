5781bb0e6887a1c91d06214e58ca042eabb397dd
Jeremy Dowe <jeremy.dowe@gmail.com>
2019-06-19 14:31:34 +1000

    Blur Logo on Load

--- portfolio/js/main.js
+++ portfolio/js/main.js
@@ -1,14 +1,23 @@
-const viewport_h = window.innerHeight || document.documentElement.clientHeight;
+const viewport_h = window.outerHeight || document.documentElement.clientHeight;
 const viewportsOnPage = document.querySelectorAll('.viewport');
 
-function viewportSetHeight() {
+function viewportSetHeightFunc() {
   for(i=0;i<viewportsOnPage.length;i++){
     viewportsOnPage[i].style.height = 978 + 'px';
   }
 };
 
+function blurFunc(element) {
+  const logo = document.querySelector(element);
+  let myTimeOut = setTimeout(function(){
+    logo.style.filter = 'blur(0px)';
+    logo.style.WebkitFilter = 'blur(0px)';
+    logo.style.opacity = '1';
+  },500);
+};
+
 //call the func.
-viewportSetHeight();
+viewportSetHeightFunc();
 
 // OnScroll event listener for sticky navigation
 window.addEventListener('scroll', function(event){
@@ -17,7 +26,7 @@ window.addEventListener('scroll', function(event){
   if (mouse_y > 580) {
       el.style.position = 'absolute';
       el.classList.add('sticky');
-      if (mouse_y <= viewport_h * 3.332) {
+      if (mouse_y <= viewport_h * 2) {
           el.style.position = 'fixed';
           el.style.top = window.innerHeight;
       }
@@ -30,7 +39,7 @@ window.addEventListener('scroll', function(event){
 window.addEventListener('resize', function(event){
   const viewportsOnPage = document.querySelectorAll('.viewport');
   viewportsOnPage.forEach(function(elems, index){
-      elems.style.height = event.srcElement.innerHeight + 'px';
+      elems.style.height = event.srcElement.outerHeight + 'px';
   });
 });
 
@@ -40,8 +49,12 @@ const darkmodeSwitch = document.querySelector('.OnOffSwitch');
 darkmodeSwitch.addEventListener('change', function(event){
 	 //event.target.checked TRUE or FALSE set the attribute on document body
    if (event.target.checked){
-     document.body.setAttribute('data-theme', 'dark')
+     document.body.setAttribute('data-theme', 'dark');
    } else {
-     document.body.removeAttribute('data-theme')
-   }
+     document.body.removeAttribute('data-theme');
+   };
+});
+
+window.addEventListener('load', function(event){
+    blurFunc('#logo');
 });
